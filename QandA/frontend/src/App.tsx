076897f13aa7975dcb691d94b09fef41468147c3
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2021-03-24 20:02:08 -0400

    chapter 4 completion

--- QandA/frontend/src/App.tsx
+++ QandA/frontend/src/App.tsx
@@ -1,11 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import React from "react";
-import "./App.css";
 import { Header } from "./Header";
 import { HomePage } from "./HomePage";
+import { fontFamily, fontSize, gray2 } from "./Styles";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
       <Header />
       <HomePage />
     </div>
